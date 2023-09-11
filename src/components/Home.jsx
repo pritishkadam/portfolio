4e8936adb18e4a6d96bb61f71d8287b1269334a0
Pritish Kadam <prtshkdm18@gmail.com>
2023-09-11 18:45:11 +0530

    update home and about section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
           </h2>
           <p className='text-white py-4 max-w-md'>
             I work as a full time Software Developer at Aureus Analytics. I love to work on web applications
-            and tinker with side projects.
+            and in free time, tinker with side projects.
           </p>
 
           <div>
