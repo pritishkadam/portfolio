3d844797f240ecb55268e463a2f1e55b53a77d22
Pritish Kadam <prtshkdm18@gmail.com>
2023-11-17 20:39:40 +0530

    (fix): update file path in socialLinksConfig

--- src/components/SocialLinks/socialLinksConfig.js
+++ src/components/SocialLinks/socialLinksConfig.js
@@ -3,52 +3,49 @@ import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const socialLinks = [
-    {
-      id: 1,
-      linkText: 'LinkedIn',
-      component: (
-        <>
-          LinkedIn <FaLinkedin size={25} />
-        </>
-      ),
-      redirectTo: 'https://www.linkedin.com/in/pritish-kadam/',
-      style: 'rounded-tr-md'
-    },
-    {
-      id: 2,
-      linkText: 'GitHub',
-      component: (
-        <>
-          GitHub <FaGithub size={25} />
-        </>
-      ),
-      redirectTo: 'https://github.com/pritishkadam',
-      
-    },
-    {
-      id: 3,
-      linkText: 'Mail',
-      component: (
-        <>
-          MailTo <HiOutlineMail size={25} />
-        </>
-      ),
-      redirectTo: 'mailto:prtshkdm18@gmail.com',
-      
-    },
-    {
-      id: 4,
-      linkText: 'Resume',
-      component: (
-        <>
-          Resume <BsFillPersonLinesFill size={25} />
-        </>
-      ),
-      redirectTo: '/pritish_resume.pdf',
-      style: 'rounded-br-md',
-      download: true
-    },
-  ];
-
+  {
+    id: 1,
+    linkText: 'LinkedIn',
+    component: (
+      <>
+        LinkedIn <FaLinkedin size={25} />
+      </>
+    ),
+    redirectTo: 'https://www.linkedin.com/in/pritish-kadam/',
+    style: 'rounded-tr-md',
+  },
+  {
+    id: 2,
+    linkText: 'GitHub',
+    component: (
+      <>
+        GitHub <FaGithub size={25} />
+      </>
+    ),
+    redirectTo: 'https://github.com/pritishkadam',
+  },
+  {
+    id: 3,
+    linkText: 'Mail',
+    component: (
+      <>
+        MailTo <HiOutlineMail size={25} />
+      </>
+    ),
+    redirectTo: 'mailto:prtshkdm18@gmail.com',
+  },
+  {
+    id: 4,
+    linkText: 'Resume',
+    component: (
+      <>
+        Resume <BsFillPersonLinesFill size={25} />
+      </>
+    ),
+    redirectTo: '/PRITISH_KADAM_RESUME.pdf',
+    style: 'rounded-br-md',
+    download: true,
+  },
+];
 
 export default socialLinks;
